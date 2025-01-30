e75069ab71f2817fb1b4daa6b4f0c327d300fff9
ggsalazar <griffingsalazar@gmail.com>
2025-01-30 00:33:35 -0600

    Rename

--- SeedsOfApocrypha/src/Entities/Items/Item.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../Entity.h"
-
-class Creature : public Entity {
-public:
-
-}
--- SeedsOfApocrypha/src/Utils/Collision.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-
-class Collision {
-public:
-    static bool AABB(const sf::FloatRect a, const sf::FloatRect b);
-};
